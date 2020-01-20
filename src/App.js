a4bd2f0343e758f2f509e054558145bc62580a45
Your Name <you@example.com>
2020-01-20 20:17:25 +0100

    first commit test technique moment: modal to do + css to finish

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [myMovieList, setMyMovieList] = useState({});
+  const [page, setPage] = useState(1);
+  // const [dataPages, setDataPages] = useState();
+
+  let link =
+    "https://api.themoviedb.org/3/movie/now_playing?api_key=f6102bdcf50d58b04b50380aa65a1652&language=fr&page=" +
+    page;
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(link);
+      setMyMovieList(response.data.results);
+      // setDataPages(response.data.total_pages);
+      setIsLoading(false);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [page]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoading === true ? (
+        <span>Chargement en cours...</span>
+      ) : (
+        <>
+          <h1>Liste de films</h1>
+          <section className="moviesList">
+            {myMovieList.map((element, index) => {
+              return (
+                <div key={element.id} className="movieCard">
+                  <div className="moviePosterContainer">
+                    <img
+                      className="moviePoster"
+                      alt="moviePoster"
+                      src={
+                        "https://image.tmdb.org/t/p/w370_and_h556_bestv2/" +
+                        element.poster_path
+                      }
+                    />
+                  </div>
+                  <div className="movieTitleContainer">
+                    <span className="movieTitle">{element.title}</span>
+                  </div>
+                </div>
+              );
+            })}
+            {/* <span
+              onClick={() => {
+                if (page > 1) {
+                  setPage(page - 1);
+                }
+              }}
+            >
+              PREV
+            </span>
+            <span
+              onClick={() => {
+                if (page < dataPages) {
+                  setPage(page + 1);
+                }
+              }}
+            >
+              NEXT
+            </span> */}
+          </section>
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default App;
